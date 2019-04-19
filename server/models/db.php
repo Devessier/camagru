16bf89a1c4a78f81b924d1df5095207d15acabb9
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-04-19 16:55:32 +0200

    Reset all the backend code

--- server/models/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function db_connect () {
-	$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWD, DB_NAME, DB_PORT);
-	if (mysqli_connect_errno())
-		return null;
-	return $connection;
-}
