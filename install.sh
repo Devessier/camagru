d26664a3c6b0268cd1bd6e71a05940713c76da11
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-05-01 13:29:21 +0200

    Use `apt-get` instead of `apt`

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-apt update
-apt install -y mysql-server
-apt install -y curl vim
+apt-get update
+apt-get install -y mysql-server
+apt-get install -y curl vim
 
-apt install apt-transport-https lsb-release
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg # Download the signing key
+apt-get install -y apt-transport-https lsb-release
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg 2>&- # Download the signing key
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list' # Add Ondrej's repo to sources list.
-apt update
+apt-get update
 
-apt install php7.3 php7.3-common php7.3-cli
-apt install php7.3-bcmath php7.3-bz2 php7.3-curl php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-readline php7.3-xml php7.3-zip
+apt-get install -y php7.3 php7.3-common php7.3-cli
+apt-get install -y php7.3-bcmath php7.3-bz2 php7.3-curl php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-readline php7.3-xml php7.3-zip
 
 curl https://getcaddy.com | bash -s personal http.cors
 
