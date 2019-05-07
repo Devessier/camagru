58e5f8dbfbe3228cc1ab0c64ff9845e5c9dca532
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-05-07 19:07:51 +0200

    Deleted useless space

--- config/fake.sql
+++ config/fake.sql
@@ -5,4 +5,3 @@ USE camagru;
 INSERT INTO users (username, password, email) VALUES ('Shakespeare', '$2y$10$pcw4JiCafsytCU8n8beB4edycWD6SgSDqZG5qFnUQeYuazcgu9yUK', 'test@example.fr'); -- password is 'Hamlet'
 INSERT INTO users (username, password, email) VALUES ('Racine', '$2y$10$3r0VbsUo/hJTmzNId0ZbBuoUYfl.Dpr2oAqJLjKo92y/w1Iij78PG', 'coucou@example.fr'); -- password is 'Andromaque'
 
-
