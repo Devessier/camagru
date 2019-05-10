c01b6a493c7e839ae11b045d2ea450eaef4ea8d4
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-05-10 17:23:30 +0200

    Created the function to modify fields of user entry

--- server/controllers/UserController.php
+++ server/controllers/UserController.php
@@ -94,4 +94,50 @@ class UserController {
         }
     }
 
+    public static function modify (Request $request) {
+        try {
+            $id = $request->session('id');
+            if ($id === null)
+                return Response::unauthorized();
+
+            $body = $request->body();
+
+            $username = $body->username ?? null;
+            $password = $body->password ?? null;
+            $email = $body->email ?? null;
+            $avatar = $body->avatar ?? null;
+
+            DB::connect();
+
+            if (is_string($username)) {
+                DB::update('UPDATE users SET username = :username WHERE id = :id', [
+                    'username' => $username,
+                    'id' => $id
+                ]);
+            }
+
+            if (is_string($password)) {
+                DB::update('UPDATE users SET password = :password WHERE id = :id', [
+                    'password' => password_hash($password, PASSWORD_BCRYPT),
+                    'id' => $id
+                ]);
+            }
+
+            if (is_string($email) && validEmail($email)) {
+                DB::update('UPDATE users SET email = :email WHERE id = :id', [
+                    'email' => $email,
+                    'id' => $id
+                ]);
+            }
+
+            return Response::make()
+                    ->json([
+                        'success' => true
+                    ]);
+        } catch (\Exception $e) {
+            echo "e : $e\n";
+            return Response::internalError();
+        }
+    }
+
 }
