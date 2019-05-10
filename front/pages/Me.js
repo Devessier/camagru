c8c9e9c0a867e52b23863a98087a8841acbf533c
Devessier <baptiste@devessier.fr>
2019-05-10 14:34:14 +0200

    List all the posts of the user

--- front/pages/Me.js
+++ front/pages/Me.js
@@ -5,10 +5,19 @@ const Me = (() => {
         posts: []
     }
 
-    function post (h, props) {
+    function post (h, props, i) {
         return h`
-            <article class="w-1/3 relative post">
-                <img src="${ props.url }" class="object-contain h-full w-full" />
+            <article class="w-1/3 border border-white">
+                <div class="relative post">
+                    <div class="absolute pin">
+                        <img src="${ props.url }" class="h-full w-full" style="object-fit: cover;" />
+                        <div class="flex justify-center items-center z-50 absolute w-full h-full pin-t bg-transparent hover:bg-black opacity-75 text-transparent hover:text-white short-transition">
+                            <svg
+                                    onclick="${ () => { data.posts.splice(i, 1) } }"
+                                    class="w-10 h-10 hover:text-red transition cursor-pointer mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                        </div>
+                    </div>
+                </div>
             </article>
         `
     }
@@ -17,7 +26,7 @@ const Me = (() => {
         console.log(props)
         return h`
             <div class="flex justify-center items-center md:mx-10 mt-10">
-                <div class="flex flex-col items-stretch w-3/5">
+                <div class="flex flex-col items-stretch w-full md:w-3/5">
                     <div class="flex justify-between flex-wrap">
                         <div class="flex flex-col justify-around">
                             <div class="flex flex-col">
@@ -38,13 +47,13 @@ const Me = (() => {
                         </div>
                     </div>
                     <section class="mt-8">
-                        <header class="pb-2 border-black border-b">
+                        <header class="pb-2">
                             <h2 class="font-medium">Mes posts</h2>
                         </header>
                         <section
                                 class="flex flex-wrap"
                         >${
-                            props.posts.map(p => post(Maverick.link(p), p))
+                            props.posts.map((p, i) => post(Maverick.link(p), p, i))
                         }</section>
                     </section>
                 </div>
