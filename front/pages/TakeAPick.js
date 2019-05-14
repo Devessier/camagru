e21efcf976cfbe9f2c15e49d797f9b17b0ec50cc
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-05-14 14:42:08 +0200

    A filter can be placed and replaced

--- front/pages/TakeAPick.js
+++ front/pages/TakeAPick.js
@@ -8,17 +8,29 @@ const TakeAPick = (() => {
 			{
 				name: 'Vue.js',
 				path: 'https://vuejs.org/images/logo.png',
-				width: '200px',
-				height: '100px'
+				width: 200,
+				height: 200
 			},
 			{
 				name: 'PHP is perfect',
 				path: 'https://whydoesitsuck.com/why-does-php-suck/thumbnail.png',
-				width: '300px',
-				height: '200px'
+				width: 300,
+				height: 300
 			}
 		],
-		filter: undefined,
+		filter: {
+			path: '',
+			name: '',
+			width: 0,
+			height: 0,
+			position: {
+				x: 0,
+				y: 0,
+				startX: 0,
+				startY: 0
+			},
+			block: false
+		},
 		photo: {
 			url: ''
 		},
@@ -36,11 +48,31 @@ const TakeAPick = (() => {
 
 		canvas.getContext('2d').drawImage(image, 0, 0, WIDTH, HEIGHT)
 		props.photo.url = canvas.toDataURL('image/png')
+		props.filter.block = true
 	}
 
 	function cancel (props) {
-		props.filter = undefined
+		props.filter = {
+			path: '',
+			name: '',
+			width: 0,
+			height: 0,
+			position: {
+				x: 0,
+				y: 0,
+				startX: 0,
+				startY: 0
+			},
+			block: false
+		}
 		props.photo.url = '' 
+	}
+
+	function pick (props) {
+		data.filter.path = props.path
+		data.filter.name = props.name
+		data.filter.width = props.width
+		data.filter.height = props.height
 	}
 
 	function stackableImage (h, props, index) {
@@ -49,7 +81,7 @@ const TakeAPick = (() => {
 				<img
 						src="${ props.path }"
 						style="object-fit: contain; height: 100px; width: 200px"
-						onclick="${ () => { data.filter = props } }"
+						onclick="${ pick.bind(this, props) }"
 				/>
 				<figcaption class="text-center">${ props.name }</figcaption>
 			</figure>
@@ -75,26 +107,53 @@ const TakeAPick = (() => {
 		`
 	}
 
+	let move = false
+
+	let rect
+	let diff = {
+		x: 0,
+		y: 0
+	}
+
 	function handleMouseDown (e, props) {
-		if (!props.filter)
+		if (props.filter.block)
 			return
-		props.filter.position = {}
-		props.filter.position.startX = e.clientX
-		props.filter.position.startY = e.clientY
+		if (!rect)
+			rect = document.getElementById('eh').getBoundingClientRect()
+		diff.x = e.clientX - rect.left - props.filter.position.x | 0
+		diff.y = e.clientY - rect.top - props.filter.position.y | 0
+		move = true
 	}
 
 	function handleMouseMove (e, props) {
-		if (!props.filter)
+		if (!move || props.filter.block)
 			return
-		props.filter.position.x = props.filter.position.startX - e.clientX
-		props.filter.position.y = props.filter.position.startY - e.clientY
-		props.filter.position.startX = e.clientX
-		props.filter.position.startY = e.clientY
+
+		const x = e.clientX - rect.left - diff.x | 0
+		const y = e.clientY - rect.top - diff.y | 0
+
+		if (x < 0)
+			props.filter.position.x = 0
+		else if ((x + props.filter.width) > rect.width)
+			props.filter.position.x = rect.width - props.filter.width
+		else
+			props.filter.position.x = x
+
+		if (y < 0)
+			props.filter.position.y = 0
+		else if ((y + props.filter.height) > rect.height)
+			props.filter.position.y = rect.height - props.filter.height
+		else
+			props.filter.position.y = y
+	}
+
+	function handleMouseUp () {
+		move = false
 	}
 
 	function render (h, props) {
-		const style = props.filter && props.filter.position && ('top: ' + props.filter.position.y + 'px; left: ' + props.filter.position.x + 'px')
-		console.log(style)
+		const style = (props.filter && props.filter.position && ('transform: translate3d(' + props.filter.position.x + 'px,' + props.filter.position.y + 'px, 0px)')) || false
+
 		return h`
 			<section class="flex flex-col items-center">
 				<header class="flex justify-center items-center my-6">
@@ -102,20 +161,27 @@ const TakeAPick = (() => {
 				</header>
 
 				<section class="flex flex-col items-center" style="width: 640px">
-					<article class="h-full w-full relative" style="height: 480px; background: linear-gradient(to right, #9796f0, #fbc7d4);">
+					<article
+							id="eh"
+
+							class="h-full w-full relative"
+							style="height: 480px; background: linear-gradient(to right, #9796f0, #fbc7d4);"
+					>
 						<video width="640" height="480" id="video" class="${ props.photo.url ? 'hidden' : 'rounded' }"></video>
 						<img id="photo" class="${ props.photo.url ? 'h-full w-full rounded flash' : 'hidden' }" src="${ props.photo.url }" />
 
 						<img
 								src="${ props.filter && props.filter.path }"
-								class="${ props.filter ? 'absolute pin-t pin-l transition' : 'hidden' }"
-								width="${ props.filter && props.filter.height }"
-								height="${ props.filter && props.filter.width }"
-
+								class="${ props.filter ? 'absolute pin-t pin-l' : 'hidden' }"
 								style="${ style }"
 
-								onmousedown="${ e => { handleMouseDown(e, props) } }"
+								ondragstart="${ () => false }"
 								onmousemove="${ e => { handleMouseMove(e, props) } }"
+								onmouseup="${ handleMouseUp.bind(null, props) }"
+								onmousedown="${ e => { handleMouseDown(e, props) } }"
+
+								width="${ props.filter.width + 'px' }"
+								height="${ props.filter.height + 'px' }"
 						/>
 					</article>
 
