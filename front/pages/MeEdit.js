6a0dd30060cfd818c31be99059f3bda1068a9936
Devessier <baptiste@devessier.fr>
2019-07-09 12:27:20 +0200

    Modified MeEdit page Try to make a better UI

--- front/pages/MeEdit.js
+++ front/pages/MeEdit.js
@@ -1,9 +1,18 @@
 const MeEdit = (() => {
 
 	let data = {
-		username: '',
-		email: '',
-		password: '',
+		username: {
+			value: '',
+			modifying: false
+		},
+		email: {
+			value: '',
+			modifying: false
+		},
+		password: {
+			value: '',
+			modifying: false
+		},
 		visible: false,
 		toast: {
 			title: '',
@@ -52,51 +61,103 @@ const MeEdit = (() => {
 			})
 	}
 
-	function render (h, props) {
+	function modifyButton (props) {
+		const render = Maverick.link()
+
+		return render`
+			<button
+					class="px-4 py-2 border rounded border-purple-lighter text-purple hover:border-purple-light hover:text-purple-dark short-transition"
+					onclick="${ () => { props.modifying = true } }"
+			>
+				Modifier
+			</button>
+		`
+	}
+
+	function saveButton (props, handler) {
+		const render = Maverick.link()
+
+		const reset = () => props.modifying = false
+		const onSave = () => {
+			reset()
+			return handler()
+		}
+
+		return render`
+			<button
+					class="px-4 py-2 border rounded border-green-lighter text-green hover:border-green-light hover:text-green-dark short-transition"
+					onclick="${ onSave }"
+			>
+				Sauvegarder
+			</button>
+			<button
+					class="px-4 py-2 border rounded border-grey-lighter text-grey hover:border-grey-light hover:text-grey-dark short-transition"
+					onclick="${ reset }"
+			>
+				Annuler
+			</button>
+		`
+	}
+
+	function profileProperty (props, index, items) {
+		const title = props.title || ''
+		const placeholder = props.placeholder || ''
+		const ref = props.ref
+
+		const render = Maverick.link(ref)
+
+		const isLast = index === items.length - 1
+		const modifying = ref.modifying
+
+		return render`
+			<div class="${ (!isLast ? 'pb-2 mb-4 border-b border-grey-light ' : '') +  'flex' }">
+				<div class="flex flex-col w-1/2">
+					<h3 class="mb-3 w-full font-medium">${
+						title
+					}</h3>
+
+					<input
+							value="${ ref.value }"
+							oninput="${ event => { ref.value = event.target.value } }"
+							placeholder="${ placeholder }"
+
+							class="mb-3 font-light border-purple-lighter focus:border-purple border-b short-transition"
+					/>
+				</div>
+
+				<div class="flex justify-end items-start w-1/2">${
+					modifying ? saveButton(ref, () => {}) : modifyButton(ref)
+				}</div>
+			</div>
+		`
+	}
+
+	function h (render, props) {
 		const eye = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>'
 		const eye_off = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>'
 
-		return h`
+		const items = [
+			{
+				title: "Votre identifiant",
+				placeholder: "Identifiant",
+				ref: props.username
+			},
+			{
+				title: "Votre adresse mail",
+				placeholder: "Adresse mail",
+				ref: props.email
+			}
+		]
+
+		return render`
 			<div class="flex justify-center items-center md:mx-10 mt-10">
 				<div class="flex flex-col w-full md:w-3/5">
-					<div class="flex justify-between flex-wrap">
-						<div class="flex flex-col justify-around">
-							<div class="flex flex-col">
-								<input
-										value="${ props.username }"
-										oninput="${ event => { props.username = event.target.value } }"
-										placeholder="Nom d'utilisateur"
-
-										class="mb-3 text-2xl font-bold border-purple-lighter focus:border-purple border-b-2 short-transition"
-								/>
-								<input
-										value="${ props.email }"
-										oninput="${ event => { props.email = event.target.value } }"
-										placeholder="Adresse mail"
-
-										class="mb-2 font-normal text-lg border-purple-lighter focus:border-purple border-b-2 short-transition"
-								/>
-								<div class="flex w-64">
-									<input
-											value="${ props.password }"
-											oninput="${ event => { props.password = event.target.value } }"
-
-											placeholder="Nouveau mot de passe"
-											type="${ !props.visible ? 'password' : 'text' }"
-
-											class="font-normal text-lg border-purple-lighter focus:border-purple border-b-2 short-transition"
-									/>
-									<div
-											onclick="${ () => { props.visible ^= 1 } }"
-											class="flex justify-center items-center"
-									>${ props.visible ? eye_off : eye }</div>
-								</div>
-							</div>
-						</div>
-						<div>
-							<img src="${ GLOBAL_STATE.avatar }" class="w-32 h-32 rounded-full" />
-						</div>
-					</div>
+					<h2 class="pb-3 mb-5 border-b border-grey-light">
+						Modification du profil
+					</h2>
+					<div class="flex flex-col items-stretch">${
+						items.map(profileProperty)
+					}</div>
 					<div class="flex justify-center md:justify-start items-center">
 						<button
 								onclick="${ () => { reset(props) } }"
@@ -114,10 +175,10 @@ const MeEdit = (() => {
 		`
 	}
 
-	return new Page('Camagru - Édition du profil', data, render, {
+	return new Page('Camagru - Édition du profil', data, h, {
 		created: function created () {
-			data.username = GLOBAL_STATE.user.username
-			data.email = GLOBAL_STATE.user.email
+			data.username.value = GLOBAL_STATE.user.username
+			data.email.value = GLOBAL_STATE.user.email
 		}
 	})
 
