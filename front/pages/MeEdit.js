5b4d8965d9001dbf7881d3f0f6030584ff731e27
Devessier <baptiste@devessier.fr>
2019-07-09 18:50:49 +0200

    Upgraded the form, we save correctly the changed properties

--- front/pages/MeEdit.js
+++ front/pages/MeEdit.js
@@ -22,43 +22,40 @@ const MeEdit = (() => {
 		}
 	}
 
-	function reset (props) {
-		props.username = GLOBAL_STATE.user.username
-		props.email = GLOBAL_STATE.user.email
-		props.password = ''
-	}
+	function save (property) {
+		return () => {
+			const body = {}
+			const value = data[property].value
 
-	function save (props) {
-		fetch('http://localhost:8001/me/modify', {
-			method: 'PUT',
-			credentials: 'include',
-			body: JSON.stringify({
-				username: props.username,
-				password: props.password,
-				email: props.email,
-				avatar: props.avatar
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then(res => res.json())
-			.then((data) => {
-				if (data.success) {
-					GLOBAL_STATE.user.username = props.username
-					GLOBAL_STATE.user.email = props.email
+			body[property] = value
+
+			fetch('http://localhost:8001/me/modify', {
+				method: 'PUT',
+				credentials: 'include',
+				body: JSON.stringify(body),
+				headers: {
+					'Content-Type': 'application/json'
 				}
-				$toast(
-					props,
-					'Édition du profil', 
-					data.success ?
-						'Votre profil a été modifié avec succès'
-						: "Une erreur s'est produite, veuillez réessayer ultérieurement"
-				)
 			})
-			.catch(() => {
-				$toast(props, 'Édition du profil', 'Une erreur a eu lieu pendant la mise à jour des données')
-			})
+				.then(res => res.json())
+				.then((response) => {
+					if (response.success) {
+						GLOBAL_STATE.user[property] = value
+					} else {
+						data[property].value = GLOBAL_STATE.user[property]
+					}
+					$toast(
+						data,
+						'Édition du profil',
+						response.success ?
+							'Votre profil a été modifié avec succès'
+							: "Une erreur s'est produite, veuillez réessayer ultérieurement"
+					)
+				})
+				.catch(() => {
+					$toast(data, 'Édition du profil', 'Une erreur a eu lieu pendant la mise à jour des données')
+				})
+		}
 	}
 
 	function modifyButton (props) {
@@ -74,25 +71,29 @@ const MeEdit = (() => {
 		`
 	}
 
-	function saveButton (props, handler) {
+	function saveButton (props, saveHandler, abortHandler) {
 		const render = Maverick.link()
 
 		const reset = () => props.modifying = false
+		const onAbort = () => {
+			reset()
+			return abortHandler()
+		}
 		const onSave = () => {
 			reset()
-			return handler()
+			return saveHandler()
 		}
 
 		return render`
 			<button
-					class="px-4 py-2 border rounded border-green-lighter text-green hover:border-green-light hover:text-green-dark short-transition"
+					class="px-4 py-2 mr-2 border rounded border-green-lighter text-green hover:border-green-light hover:text-green-dark short-transition"
 					onclick="${ onSave }"
 			>
 				Sauvegarder
 			</button>
 			<button
 					class="px-4 py-2 border rounded border-grey-lighter text-grey hover:border-grey-light hover:text-grey-dark short-transition"
-					onclick="${ reset }"
+					onclick="${ onAbort }"
 			>
 				Annuler
 			</button>
@@ -109,6 +110,10 @@ const MeEdit = (() => {
 		const isLast = index === items.length - 1
 		const modifying = ref.modifying
 
+		const onAbort = () => {
+			ref.value = GLOBAL_STATE.user[props.property || ''] || ''
+		}
+
 		return render`
 			<div class="${ (!isLast ? 'pb-2 mb-4 border-b border-grey-light ' : '') +  'flex' }">
 				<div class="flex flex-col w-1/2">
@@ -120,32 +125,32 @@ const MeEdit = (() => {
 							value="${ ref.value }"
 							oninput="${ event => { ref.value = event.target.value } }"
 							placeholder="${ placeholder }"
+							disabled="${ !modifying }"
 
-							class="mb-3 font-light border-purple-lighter focus:border-purple border-b short-transition"
+							class="mb-3 pb-1 font-light border-purple-lighter focus:border-purple border-b short-transition account-input"
 					/>
 				</div>
 
 				<div class="flex justify-end items-start w-1/2">${
-					modifying ? saveButton(ref, () => {}) : modifyButton(ref)
+					modifying ? saveButton(ref, save(props.property), onAbort) : modifyButton(ref)
 				}</div>
 			</div>
 		`
 	}
 
 	function h (render, props) {
-		const eye = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>'
-		const eye_off = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>'
-
 		const items = [
 			{
 				title: "Votre identifiant",
 				placeholder: "Identifiant",
-				ref: props.username
+				ref: props.username,
+				property: 'username'
 			},
 			{
 				title: "Votre adresse mail",
 				placeholder: "Adresse mail",
-				ref: props.email
+				ref: props.email,
+				property: 'email'
 			}
 		]
 
@@ -158,18 +163,6 @@ const MeEdit = (() => {
 					<div class="flex flex-col items-stretch">${
 						items.map(profileProperty)
 					}</div>
-					<div class="flex justify-center md:justify-start items-center">
-						<button
-								onclick="${ () => { reset(props) } }"
-								class="text-grey-darker border-grey-light hover:border-grey border-2 rounded px-3 py-2 mr-4 short-transition">
-							Annuler
-						</button>
-						<button
-								onclick="${ () => { save(props) } }"
-								class="text-purple-dark border-purple-light hover:border-purple border-2 rounded px-3 py-2 short-transition">
-							Sauvegarder
-						</button>
-					</div>
 				</div>
 			</div>
 		`
--- server/controllers/UserController.php
+++ server/controllers/UserController.php
@@ -35,45 +35,40 @@ class UserController {
     public static function modify (Request $request) {
         try {
             $id = $request->session('id');
-            if ($id === null)
-                return Response::unauthorized();
 
             $body = $request->body();
 
             $username = $body->username ?? null;
             $password = $body->password ?? null;
             $email = $body->email ?? null;
-            $avatar = $body->avatar ?? null;
 
-            DB::connect();
+            $done = false;
 
             if (is_string($username)) {
+                DB::connect();
+
                 DB::update('UPDATE users SET username = :username WHERE id = :id', [
                     'username' => $username,
                     'id' => $id
                 ]);
-            }
 
-            if (is_string($password)) {
-                DB::update('UPDATE users SET password = :password WHERE id = :id', [
-                    'password' => password_hash($password, PASSWORD_BCRYPT),
-                    'id' => $id
-                ]);
-            }
+                $done = true;
+            } else if (is_string($email) && validEmail($email)) {
+                DB::connect();
 
-            if (is_string($email) && validEmail($email)) {
                 DB::update('UPDATE users SET email = :email WHERE id = :id', [
                     'email' => $email,
                     'id' => $id
                 ]);
+
+                $done = true;
             }
 
             return Response::make()
                     ->json([
-                        'success' => true
+                        'success' => $done
                     ]);
         } catch (\Exception $e) {
-            echo "e : $e\n";
             return Response::internalError();
         }
     }
