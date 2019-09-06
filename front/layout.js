7176d6f29188306af087a6b93dd133ac192b9df3
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-06 13:46:26 +0200

    Add a footer

--- front/layout.js
+++ front/layout.js
@@ -36,38 +36,38 @@ const NotAuthenticatedHeader = (h) => (
     `
 )
 
-function layout (h, props) {
+function layout (render, props) {
     const fn = isAuthenticated() ? AuthenticatedHeader : NotAuthenticatedHeader
 
     const headerRenderer = Maverick.link(fn)
     const pageRenderer = Maverick.link(props._page)
     const toastRenderer = Maverick.link(props.toast)
 
-    return h`<nav class="flex flex-wrap items-center justify-between bg-purple-light px-3 md:px-12 py-4">
-        <h1>
-            <a href="/" onclick="${ router.click('/') }" class="font-semibold">
-                <span class="block sm:hidden text-white">Cam</span>
-                <span class="hidden sm:block text-white">Camagru</span>
-            </a>
-        </h1>
-        <div class="sm:hidden">
-            <button class="px-2 py-1 flex flex-col justify-center items-center border border-grey-darkest rounded" onclick="${ () => { GLOBAL_STATE.layout.menu.open ^= 1 } }">
-                <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="fill-current h-5 w-5"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-        </div>
-        <div
-                class="${ 'sm:flex flex-row justify-between items-center w-full sm:w-auto ' + (GLOBAL_STATE.layout.menu.open ? 'block' : 'hidden') }"
-        >${
-            fn(headerRenderer)
-        }</div>
-    </nav>
-    <main>${
-        props._page(pageRenderer, props)
-    }</main>
-    <aside>${
-        toast(toastRenderer, props)
-    }</aside>
-    <footer>
-        
-    </footer>`
+    return render`
+        <nav class="flex flex-wrap items-center justify-between bg-purple-light px-3 md:px-12 py-4">
+            <h1>
+                <a href="/" onclick="${ router.click('/') }" class="font-semibold">
+                    <span class="block sm:hidden text-white">Cam</span>
+                    <span class="hidden sm:block text-white">Camagru</span>
+                </a>
+            </h1>
+            <div class="sm:hidden">
+                <button class="px-2 py-1 flex flex-col justify-center items-center border border-grey-darkest rounded" onclick="${ () => { GLOBAL_STATE.layout.menu.open ^= 1 } }">
+                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="fill-current h-5 w-5"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div
+                    class="${ 'sm:flex flex-row justify-between items-center w-full sm:w-auto ' + (GLOBAL_STATE.layout.menu.open ? 'block' : 'hidden') }"
+            >${ fn(headerRenderer) }</div>
+        </nav>
+        <main>${ props._page(pageRenderer, props) }</main>
+        <aside>${ toast(toastRenderer, props) }</aside>
+        <footer class="flex justify-center bg-purple-lighter py-4 px-2">
+            <div>
+                <h3 class="text-2xl text-white">
+                    Camagru
+                </h3>
+            </div>
+        </footer>
+    `
 }
