c6fac7be49ea262e6e08575336b35a14e5fc097c
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-06 14:16:35 +0200

    Delete old router code

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
 
 require __DIR__ . '/server/app.php';
-
-/*require_once __DIR__ . '/constants.php';
-require_once __DIR__ . '/router/router.php';
-require_once __DIR__ . '/router/middlewares.php';
-
-session_start();
-
-get('/^\/$/', '/controllers/index.php');
-
-get('/^\/sign-in\/?$/', '/controllers/sign-in.php', 'isNotAuthenticated');
-post('/^\/sign-in\/?$/', '/controllers/sign-in.php', 'isNotAuthenticated');
-
-get('/^\/sign-up\/?$/', '/controllers/sign-up.php', 'isNotAuthenticated');
-post('/^\/sign-up\/?$/', '/controllers/sign-up.php', 'isNotAuthenticated');
-
-get('/^\/products\/(id)\/(.+)\/?$/', '/controllers/products.php');
-
-//post('/\//', '/views/index.php', body_json);
-
-get('/^\/(.*).css$/', '/static');
-
-get('/^\/401$/', '/views/401.php');
-get('/^\/404$/', '/views/404.php');
-get('/^\/500$/', '/views/500.php');
-
-get('//', '/views/404.php');*/
