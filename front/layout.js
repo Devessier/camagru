3bd743c4a3b68292bcf8d6ff347b65a148636130
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-10 10:54:18 +0200

    Use ES5 functions !

--- front/layout.js
+++ front/layout.js
@@ -1,5 +1,5 @@
-const AuthenticatedHeader = (h) => (
-    h`
+function AuthenticatedHeader (render) {
+    return render`
         <a
                 href="/me"
                 onclick="${ router.click('/me') }"
@@ -15,10 +15,10 @@ const AuthenticatedHeader = (h) => (
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
         </button>
     `
-)
+}
 
-const NotAuthenticatedHeader = (h) => (
-    h`
+function NotAuthenticatedHeader (render) {
+    return render`
         <a
                 href="/sign-up"
                 onclick="${ router.click('/sign-up') }"
@@ -34,7 +34,7 @@ const NotAuthenticatedHeader = (h) => (
             Connexion
         </a>
     `
-)
+}
 
 function layout (render, props) {
     const fn = isAuthenticated() ? AuthenticatedHeader : NotAuthenticatedHeader
